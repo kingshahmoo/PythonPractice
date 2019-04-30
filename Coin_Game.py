987be0d68197cdb0bb7351ab4e632812f975fe69
kingshahmoo <shahmerangilani@gmail.com>
2019-04-29 22:05:27 -0600

    version0.1
    First commit
    
    added areas for user input
    added information for user

---
+++ Coin_Game.py
@@ -0,0 +1,14 @@
+#For random number generator
+import random
+
+#generate random number
+random_number = int
+random_number = random.randint(1,98+1)
+
+#greet user & display rules and number
+print("Game Session Starts \n")
+print("Enter coins values as 1-penny, 5-nickel, 10-dime, and 25-quarter.\n")
+print("Enter coins that add up to ",random_number," cents, one per line.")
+
+
+
